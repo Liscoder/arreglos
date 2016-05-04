23ab3949c10558a9bc2186f2f6d1ab3b575e2911
Liscoder <lizethlopezzamudio@gmail.com>
2016-05-04 12:27:52 -0500

    terminando arrays

--- 4js_arreglos.js
+++ 4js_arreglos.js
@@ -1,15 +1,20 @@
-//**********PREGUNTA #3
+//**********PREGUNTA #4
 function mayor(){ 
-var mayor=[];
-var valores = [true, 5, false, "hola", "adios", 2];
-for(i=0; i<valores.length;i++){
-if(valores[i]<valores[i+1]){
-	mayor=valores[i+1];
-}
+	var mayor=0;
+	var valores = [true, 5, false, "hola", "adios", 2];
+	var nueva=[];
 
-}
-document.write("El valor mayor es: "+mayor);
+for(var i=0; i<valores.length-1;i++){
+	if((typeof valores[i] )== 'string') {
+		nueva.push(valores[i]);
+		}
+	for(j=0;j<nueva.length-1;j++){
+		if(nueva[j+1].length>nueva[j].length){
+			document.write("EL valor mayor: "+nueva[j+1]);
+		}else{
+			document.write("El valor mayor es: "+nueva[j]);
+		}
+	}
 
-
-
+}	
 }
